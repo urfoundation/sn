399d0354344d2adefda50180bee35abdea40131f
Product Builder <builder@bringyour.com>
2026-07-06 21:38:41 -0700

    2026.7.6-985989570

---
+++ v2026/cli/validator/main.go
@@ -0,0 +1,14 @@
+// Command validator is the thin CLI entry point for the validator library
+// (github.com/urfoundation/sn/validator). All logic lives in the library so it is
+// importable and testable; this executable only forwards os.Args.
+package main
+
+import (
+	"os"
+
+	"github.com/urfoundation/sn/v2026/validator"
+)
+
+func main() {
+	validator.Run(os.Args[1:])
+}
